b79adeca4aca2bdf8669404d7ee19bcef5f375ac
JosephSusik <joseph.susik@gmail.com>
2025-10-07 13:58:56 +0200

    fixes

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -61,7 +61,7 @@ function SheetContent({
       <SheetPrimitive.Content
         data-slot="sheet-content"
         className={cn(
-          "pointer-events-auto bg-background data-[state=open]:animate-in data-[state=closed]:animate-out fixed z-50 flex flex-col gap-4 shadow-lg transition ease-in-out data-[state=closed]:duration-300 data-[state=open]:duration-500",
+          "pointer-events-auto bg-background data-[state=open]:animate-in data-[state=closed]:animate-out fixed z-50 flex bg-white flex-col gap-4 shadow-lg transition ease-in-out data-[state=closed]:duration-300 data-[state=open]:duration-500",
           side === "right" &&
             "data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right right-0 bottom-0 h-full  h-[calc(100vh-56px)] w-3/4 border-l sm:max-w-sm",
           side === "left" &&
