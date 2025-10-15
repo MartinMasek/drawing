708a8ac13cf8c85b892d67e8b27fb9799f6e8d7a
JosephSusik <joseph.susik@gmail.com>
2025-10-15 18:02:43 +0200

    rm unused file

--- src/components/sidePanelContent/sidePanelDimensions/components/MaterialTileSkeleton.tsx
+++
@@ -1,7 +0,0 @@
-import type { FC } from "react";
-
-const MaterialTileSkeleton: FC = () => {
-	return <div className="h-[191px] w-[304px] rounded-md bg-gray-200" />;
-};
-
-export default MaterialTileSkeleton;
